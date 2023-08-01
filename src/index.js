299663fc4c2e26e598995a0b3ecd5ca52a832988
JorgeCordero666 <jorge666.cordero@gmail.com>
2023-08-01 12:13:11 -0500

    Add new index

--- src/index.js
+++ src/index.js
@@ -1,32 +1,65 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import dotenv from 'dotenv';
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, collection, addDoc } from "firebase/firestore/lite";
 
-dotenv.config();
-
-
+// TODO: Replace the following with your appFirebase's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDTUCQmEbojSajLLk2FRJOsAaxPpXC5ID0",
-    authDomain: "basededatossignup.firebaseapp.com",
-    projectId: "basededatossignup",
-    storageBucket: "basededatossignup.appspot.com",
-    messagingSenderId: "238092592594",
-    appId: "1:238092592594:web:e4ab82eef1203cc2bcd40b"
-  };
+  apiKey: "AIzaSyDTUCQmEbojSajLLk2FRJOsAaxPpXC5ID0",
+  authDomain: "basededatossignup.firebaseapp.com",
+  projectId: "basededatossignup",
+  storageBucket: "basededatossignup.appspot.com",
+  messagingSenderId: "238092592594",
+  appId: "1:238092592594:web:e4ab82eef1203cc2bcd40b"
+};
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
 
-  firebase.initializeApp(firebaseConfig);
+// Función para registrar al usuario en Firebase Authentication y agregarlo a Firestore
+async function registerUserAndAddToFirestore(email, password) {
+  try {
+    // Inicializa la autenticación de Firebase y obtén una referencia al servicio
+    const auth = getAuth();
 
-  document.getElementById('signup-form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-  
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        console.log('User registered:', userCredential.user);
-      })
-      .catch((error) => {
-        console.error('Error registering user:', error);
-      });
-  });
-  
+    // Registra al usuario en Firebase Authentication
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+
+    console.log("Usuario registrado exitosamente:", user);
+    // Muestra un mensaje emergente de éxito
+    alert("Usuario registrado exitosamente.");
+    // Limpia los campos del formulario
+    const signupForm = document.getElementById("signupForm");
+    signupForm.reset();
+
+    // Guarda la información del usuario en Firestore Lite
+    const db = getFirestore(app);
+    const usersCollection = collection(db, "users");
+    await addDoc(usersCollection, {
+      uid: user.uid,
+      email: user.email,
+    });
+    console.log("Usuario agregado a Firestore Lite");
+  } catch (error) {
+    console.error("Error durante el registro y/o agregado a Firestore:", error);
+  }
+}
+
+// Función para manejar el evento de envío del formulario
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.email.value;
+  const password = form.password.value;
+
+  // Llamada a la función para registrar al usuario y agregarlo a Firestore
+  registerUserAndAddToFirestore(email, password);
+}
+
+// Agrega un evento al formulario para manejar el envío
+const signupForm = document.getElementById("signupForm");
+signupForm.addEventListener("submit", handleFormSubmit);
+
